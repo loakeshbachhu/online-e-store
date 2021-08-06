e63ae8e1a483fe1f0d3660704d3f5a76dcb5323f
Loakesh Bachhu <bachhu.loakesh@gmail.com>
2021-08-05 23:43:26 -0500

    updated product detail redux

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectedProduct } from '../redux/actions/productActions';
 
 const ProductDetails = () => {
+    const product = useSelector((state) => state.product );
+    const {productId} = useParams();
+    const dispatch = useDispatch();
+    console.log(productId);
+
+
+    const fetchProductDetail = async () => {
+        const response = await axios.get(`https://fakestoreapi.com/products/${productId}`)
+        .catch((err) => {
+            console.log("Err",err);
+        });
+        dispatch(selectedProduct(response.data));
+    };
+
+    useEffect(() => {
+        if(productId && productId !== "") fetchProductDetail()
+    },[productId]);
+
     return(
     <div>
         <h1>ProductDetails</h1>  
