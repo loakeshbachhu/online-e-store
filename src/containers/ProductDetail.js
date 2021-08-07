448c7aec60dcc2ef4c33c2d06f8ad96ad2a99c12
Loakesh Bachhu <bachhu.loakesh@gmail.com>
2021-08-06 22:22:34 -0500

    fetch products details

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -2,10 +2,11 @@ import React, {useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectedProduct } from '../redux/actions/productActions';
+import { selectedProduct, removeSelectedProduct } from '../redux/actions/productActions';
 
 const ProductDetails = () => {
     const product = useSelector((state) => state.product );
+    const {image, title, price, category, description} = product;
     const {productId} = useParams();
     const dispatch = useDispatch();
     console.log(productId);
@@ -20,13 +21,43 @@ const ProductDetails = () => {
     };
 
     useEffect(() => {
-        if(productId && productId !== "") fetchProductDetail()
+        if(productId && productId !== "") fetchProductDetail();
+        return () => {
+            dispatch(removeSelectedProduct());
+        }
     },[productId]);
 
     return(
-    <div>
-        <h1>ProductDetails</h1>  
-    </div>
+        <div className="ui grid container">
+        {Object.keys(product).length === 0 ? (
+          <div>...Loading</div>
+        ) : (
+          <div className="ui placeholder segment">
+            <div className="ui two column stackable center aligned grid">
+              <div className="ui vertical divider">AND</div>
+              <div className="middle aligned row">
+                <div className="column lp">
+                  <img className="ui fluid image" src={image} />
+                </div>
+                <div className="column rp">
+                  <h1>{title}</h1>
+                  <h2>
+                    <a className="ui teal tag label">${price}</a>
+                  </h2>
+                  <h3 className="ui brown block header">{category}</h3>
+                  <p>{description}</p>
+                  <div className="ui vertical animated button" tabIndex="0">
+                    <div className="hidden content">
+                      <i className="shop icon"></i>
+                    </div>
+                    <div className="visible content">Add to Cart</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     );
 };
 
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -17,9 +17,9 @@ export const productReducer = (state = initialState, {type,payload}) => {
 export const selectedProductReducer = (state={}, {type,payload}) => {
     switch (type) {
         case ActionTypes.SELECTED_PRODUCT:
-            
-            return {...state, ...payload};
-    
+           return {...state, ...payload};
+        case ActionTypes.REMOVE_SELECTED_PRODUCT:
+            return {};
         default:
             return state;
     }
